b8db4fded230f344a8e9a02d1f5561f733a53e5e
Gabriel Gallo <gabriel.gallo.jr@gmail.com>
2025-11-25 03:50:27 -0800

    Added sessions app to track session data

---
+++ sessions/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# This is going to represent the info on a study session
+
+class Sessions(models.Model):
+    # We should think about data we want to store for a study session
+    # ie. Start time, End time, total time, date 
+    # Possible: Study Subject, Goal study time etc
+    pass
