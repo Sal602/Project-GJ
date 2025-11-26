b1d368dd62b61414a044aaf46e9f4feb6498f1a3
Gabriel Gallo <gabriel.gallo.jr@gmail.com>
2025-11-26 02:09:05 -0800

    Updated the study_session model

--- study_session/models.py
+++ study_session/models.py
@@ -6,4 +6,37 @@ class study_session(models.Model):
     # We should think about data we want to store for a study session
     # ie. Start time, End time, total time, date 
     # Possible: Study Subject, Goal study time etc
-    pass
+    date = models.DateField(auto_now_add=True)
+    start_time = models.DateTimeField(auto_now_add=True)
+    end_time = models.DateTimeField(null=True, blank=True)
+    
+    study_subject = models.CharField(max_length=20)
+
+    total_time = models.DurationField(null=True, blank=True)
+    goal_time = models.DurationField()
+
+    goal_passed = models.BooleanField(default=False)
+
+    def studyDurationSec(self):
+        if self.total_time:
+            return int(self.total_time.total_seconds() / 60)
+        return None
+    
+    def save(self, *args, **kwargs):
+        self.get_total_time()
+
+        if self.total_time >= self.goal_time:
+            self.goal_passed = True
+        else:
+            self.goal_passed = False
+
+        super().save(*args, **kwargs)
+    
+    def get_total_time(self):
+        if self.end_time:
+            self.total_time = self.end_time - self.start_time
+            
+        
+
+
+
